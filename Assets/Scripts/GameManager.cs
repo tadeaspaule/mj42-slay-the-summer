3fa110fe78d8d041a694a27d2e74e788c34ac227
Tadeas Paule <tpaule.personal@gmail.com>
2019-11-29 10:48:58 +0000

    more gameUI and enemy spawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject characterPrefab;
+    public Transform enemyHolder;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +15,25 @@ public class GameManager : MonoBehaviour
 
     public void UseCard(Card card)
     {
-        
+
+    }
+
+    float space = 2.4f;
+
+    public void SpawnEnemies()
+    {
+        foreach (Transform child in enemyHolder) Destroy(child.gameObject);
+        int n = Random.Range(1,4);
+        List<GameObject> enemies = new List<GameObject>();
+        for (int i = 0; i < n; i++) enemies.Add(Instantiate(characterPrefab,Vector3.zero,Quaternion.identity,enemyHolder));
+        foreach (GameObject go in enemies) go.transform.localPosition = Vector3.zero;
+        if (n == 2) {
+            enemies[0].transform.position -= new Vector3(space*0.5f,0f,0f);
+            enemies[1].transform.position += new Vector3(space*0.5f,0f,0f);
+        }
+        else if (n == 3) {
+            enemies[0].transform.position -= new Vector3(space,0f,0f);
+            enemies[2].transform.position += new Vector3(space,0f,0f);
+        }
     }
 }
