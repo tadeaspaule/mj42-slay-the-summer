c6af97982f14c9be12e9faef3e6d328cb350bf83
Tadeas Paule <tpaule.personal@gmail.com>
2019-11-29 09:39:26 +0000

    basic card hand + preview

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public CardHolder holder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Mouse Interaction
+
+    public void MouseEntered()
+    {
+        holder.StartPreview(this);
+    }
+
+    public void MouseLeft()
+    {
+        holder.EndPreview();        
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/CardHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardHolder : MonoBehaviour
+{
+    public GameObject cardPrefab;
+    public Card previewCard;
+    Transform cardHand;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardHand = transform.GetChild(0);
+        for (int i = 0; i < 3; i++) AddCard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCard()
+    {
+        GameObject go = Instantiate(cardPrefab,Vector3.zero,Quaternion.identity,cardHand);
+        Card c = go.GetComponent<Card>();
+        c.holder = this;
+    }
+
+    public void StartPreview(Card card)
+    {
+        previewCard.gameObject.SetActive(true);
+    }
+
+    public void EndPreview()
+    {
+        previewCard.gameObject.SetActive(false);
+    }
+}
