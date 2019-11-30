88c30f3af508003654dc8cd07ce0623d30d9d0e0
Tadeas Paule <tpaule.personal@gmail.com>
2019-11-30 10:11:01 +0000

    main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -240,7 +241,7 @@ public class GameManager : MonoBehaviour
 
     public void ClickedMainMenu()
     {
-
+        SceneManager.LoadScene(0);
     }
 
     #endregion
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject outline;
+    
+    public void MouseEntered()
+    {
+        outline.SetActive(true);
+    }
+
+    public void MouseLeft()
+    {
+        outline.SetActive(false);
+
+    }
+}
